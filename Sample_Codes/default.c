3b795e3b1faefedac68748e23a7cee7b9230b918
Vikrant Sugandhi <vikrant.sugandhi>
2025-04-21 16:04:26 +0530

    added makefile

---
+++ Sample_Codes/default.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+#define EXIT_SUCCESS	0
+int main()
+{
+	printf("HI Default.c executed\n");
+	printf("type command as below\n");
+	printf("make TARGET=<file>.c\n");
+	return EXIT_SUCCESS;
+}
